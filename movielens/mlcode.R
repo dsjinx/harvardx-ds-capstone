333a32c265e94808e6136910530ba0a11c5def2b
J <1292069740@qq.com>
2022-07-22 19:39:14 +0800

    modified:   movielens/mlcode.R 	deleted:    draft.R

--- movielens/mlcode.R
+++ movielens/mlcode.R
@@ -4,9 +4,9 @@
 
 # Note: this process could take a couple of minutes
 
-#if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-#if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
-#if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(caret)
